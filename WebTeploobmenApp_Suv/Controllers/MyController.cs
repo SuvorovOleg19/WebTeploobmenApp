c49664206af232f7de6cb81dd452d9ccbba278a9
Суворов Олег <oleg.suvorov.0404@mail.ru>
2024-12-08 18:19:27 +0500

    Добавили MyController.cs и страницу Test

---
+++ WebTeploobmenApp_Suv/Controllers/MyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTeploobmenApp_Suv.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
